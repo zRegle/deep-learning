88d658d5729d9934a24b6377d6a606c6e2d1f2c9
zregle <zregle@163.com>
2020-05-04 22:55:54 +0800

    first commit

---
+++ digit-recognition/digits.py
@@ -0,0 +1,102 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+import torch.utils.data.dataloader as dataloader
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(1, 6, 5, 1, 2),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(6, 16, 5),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2)
+        )
+        self.fc1 = nn.Sequential(
+            nn.Linear(16 * 5 * 5, 120),
+            nn.ReLU()
+        )
+        self.fc2 = nn.Sequential(
+            nn.Linear(120, 84),
+            nn.ReLU()
+        )
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = x.view(-1, self.num_flat_features(x))
+        # x = x.view(x.size()[0], -1)
+        x = self.fc1(x)
+        x = self.fc2(x)
+        x = self.fc3(x)
+        return x
+
+    def num_flat_features(self, x):
+        size = x.size()[1:]
+        num_features = 1
+        for s in size:
+            num_features *= s
+        return num_features
+
+
+BATCH_SIZE = 64
+EPOCH = 15
+log_interval = 500
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+trans = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.1307,), (0.3081,))
+])
+data_train = datasets.MNIST(root='data', transform=trans, train=True, download=True)
+data_test = datasets.MNIST(root='data', transform=trans, train=False, download=True)
+train_loader = dataloader.DataLoader(data_train, batch_size=BATCH_SIZE, shuffle=True, num_workers=4)
+test_loader = dataloader.DataLoader(data_test, batch_size=BATCH_SIZE, shuffle=True, num_workers=4)
+model = Net().to(device)
+optimizer = optim.SGD(model.parameters(), lr=0.01)
+
+
+def train(epoch):
+    model.train()  # 设置为train模式
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()  # 优化器梯度初始化为零
+        output = model(data)  # 把数据输入网络并得到输出，即进行前向传播
+        loss = F.cross_entropy(output, target)   # 交叉熵损失函数
+        loss.backward()  # 反向传播梯度
+        optimizer.step()  # 结束一次前传+反传之后，更新参数
+        if batch_idx % log_interval == 0:  # 准备打印相关信息
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item()))
+
+
+def test():
+    model.eval()  # 设置为test模式
+    test_loss = 0  # 初始化测试损失值为0
+    correct = 0  # 初始化预测正确的数据个数为0
+    for data, target in test_loader:
+        data, target = data.to(device), target.to(device)
+        output = model(data)
+        test_loss += F.cross_entropy(output, target, reduction='sum')  # sum up batch loss 把所有loss值进行累加
+        pred = output.data.max(1, keepdim=True)[1]  # get the index of the max log-probability
+        correct += pred.eq(target.data.view_as(pred)).cpu().sum()  # 对预测正确的数据个数进行累加
+
+    test_loss /= len(test_loader.dataset)  # 因为把所有loss值进行过累加，所以最后要除以总得数据长度才得平均loss
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+
+
+if __name__ == '__main__':
+    for e in range(1, EPOCH + 1):
+        train(e)
+        test()
+    torch.save(model, 'model.pth')
